3eda5e8eb2aae663346a51158a5c22c3a51e26c3
Jordan Marine <zeotni.jordan@gmail.com>
2019-07-01 23:14:29 -0500

    Minus not plus idiot.

--- SquidDraftLeague/SquidDraftLeague.Draft/Lobby.cs
+++ SquidDraftLeague/SquidDraftLeague.Draft/Lobby.cs
@@ -29,8 +29,6 @@ namespace SquidDraftLeague.Draft
 
                 try
                 {
-
-
                     return Math.Round(
                         this.players.Where(e => e.DiscordId != this.Halved?.DiscordId).Select(e => e.PowerLevel).Average(), 2);
                 }
