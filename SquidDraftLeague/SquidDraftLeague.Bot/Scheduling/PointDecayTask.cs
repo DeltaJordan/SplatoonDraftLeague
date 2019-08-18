c0d3563953249c94458f784ee22370c1a39c81c5
Jordan Marine <zeotni.jordan@gmail.com>
2019-08-18 11:42:50 -0500

    Fixed formula not accounting for 0 weeks of inactivity. Added try catch statement to continue point decay even if a player causes an error.

--- SquidDraftLeague/SquidDraftLeague.Bot/Scheduling/PointDecayTask.cs
+++ SquidDraftLeague/SquidDraftLeague.Bot/Scheduling/PointDecayTask.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using NLog;
 using SquidDraftLeague.AirTable;
 using SquidDraftLeague.Bot.Scheduling.Services;
 using SquidDraftLeague.Draft;
@@ -16,6 +17,9 @@ namespace SquidDraftLeague.Bot.Scheduling
     public class PointDecayTask : IScheduledTask
     {
         public string Schedule => "0 16 * * WED";
+
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
             SdlPlayer[] allSdlPlayers = await AirTableClient.RetrieveAllSdlPlayers();
@@ -23,38 +27,48 @@ namespace SquidDraftLeague.Bot.Scheduling
 
             foreach (SdlPlayer sdlPlayer in allSdlPlayers)
             {
-                PlayerActivity playerActivity;
-                string playerFile = Path.Combine(activityDirectory, $"{sdlPlayer.DiscordId}.json");
+                try
+                {
+                    PlayerActivity playerActivity;
+                    string playerFile = Path.Combine(activityDirectory, $"{sdlPlayer.DiscordId}.json");
 
-                if (File.Exists(playerFile))
-                {
-                    playerActivity =
-                        JsonConvert.DeserializeObject<PlayerActivity>(await File.ReadAllTextAsync(playerFile));
-                }
-                else
-                {
-                    playerActivity = new PlayerActivity
+                    if (File.Exists(playerFile))
                     {
-                        PlayedSets = new List<DateTime>(),
-                        Timeouts = new List<DateTime>()
-                    };
+                        playerActivity =
+                            JsonConvert.DeserializeObject<PlayerActivity>(await File.ReadAllTextAsync(playerFile));
+                    }
+                    else
+                    {
+                        playerActivity = new PlayerActivity
+                        {
+                            PlayedSets = new List<DateTime>(),
+                            Timeouts = new List<DateTime>()
+                        };
+                    }
+
+                    int timeouts = playerActivity.Timeouts.Select(e => e.Date).Distinct()
+                        .Count(e => e.Date > DateTime.UtcNow.Date - TimeSpan.FromDays(7));
+
+                    timeouts = Math.Min(7, timeouts);
+
+                    DateTime lastSetDateTime = await AirTableClient.GetDateOfLastSet(sdlPlayer);
+                    int inactiveWeeks = (DateTime.UtcNow - lastSetDateTime).Days / 7;
+
+                    if (inactiveWeeks == 0)
+                        continue;
+
+                    // Constrain to 1-4 weeks
+                    inactiveWeeks = Math.Min(Math.Max(inactiveWeeks, 1), 4);
+
+                    double decay = (7 - timeouts) * Math.Pow(15D * Math.Pow(sdlPlayer.PowerLevel, 2) / 4840000,
+                                       Math.Pow(1.2, inactiveWeeks - 1)) / 7;
+
+                    await AirTableClient.PenalizePlayer(sdlPlayer.DiscordId, decay, "Point decay.");
                 }
-
-                int timeouts = playerActivity.Timeouts.Select(e => e.Date).Distinct()
-                    .Count(e => e.Date > DateTime.UtcNow.Date - TimeSpan.FromDays(7));
-
-                timeouts = Math.Min(7, timeouts);
-
-                DateTime lastSetDateTime = await AirTableClient.GetDateOfLastSet(sdlPlayer);
-                int inactiveWeeks = (DateTime.UtcNow - lastSetDateTime).Days / 7;
-
-                // Constrain to 1-4 weeks
-                inactiveWeeks = Math.Min(Math.Max(inactiveWeeks, 1), 4);
-
-                double decay = (7 - timeouts) * Math.Pow(15D * Math.Pow(sdlPlayer.PowerLevel, 2) / 4840000,
-                                   Math.Pow(1.2, inactiveWeeks - 1)) / 7;
-
-                await AirTableClient.PenalizePlayer(sdlPlayer.DiscordId, decay, "Point decay.");
+                catch (Exception e)
+                {
+                    Logger.Error(e);
+                }
             }
         }
     }
